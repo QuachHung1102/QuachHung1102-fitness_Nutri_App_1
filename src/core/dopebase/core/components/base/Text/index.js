268ef93d6944bc73eec3d2fd220d5d9a95e5e73a
QuachHung1102 <quachhung389@gmail.com>
2024-06-25 22:55:26 +0700

    big update 25/06/2024

---
+++ src/core/dopebase/core/components/base/Text/index.js
@@ -0,0 +1,3 @@
+// Tùy thuộc vào có bn component con thì sẽ import vào đây
+import Text from './Text'
+export { Text }
--- src/core/dopebase/core/theming/index.js
+++ src/core/dopebase/core/theming/index.js
@@ -1,6 +1,6 @@
-import React, { Children } from "react";
-import { useColorScheme } from 'react-native'
-import DNDefaultTheme from './defaults'
+import React from 'react';
+import { useColorScheme } from 'react-native';
+import DNDefaultTheme from './default';
 
 export default DNDefaultTheme;
 
@@ -9,7 +9,7 @@ export const DopebaseContext = React.createContext();
 const defaultProps = {
   children: null,
   theme: {},
-}
+};
 
 export function DopebaseProvider(props = defaultProps) {
   const { theme, children } = props;
@@ -17,6 +17,43 @@ export function DopebaseProvider(props = defaultProps) {
   const overridenTheme = { ...DNDefaultTheme, ...theme };
   const context = {
     theme: overridenTheme,
-    colorScheme,
-  }
-}
+    appearance: colorScheme,
+  };
+  return (
+    <DopebaseContext.Provider value={context}>
+      {children}
+    </DopebaseContext.Provider>
+  );
+};
+
+export function useDopebase(Component, styles) {
+  return props => {
+    const colorScheme = useColorScheme();
+    return (
+      <DopebaseContext.Consumer>
+        {context => (
+          <Component
+            {...props}
+            theme={{ ...DNDefaultTheme, ...context.theme }}
+            appearance={colorScheme || context.appearance}
+            styles={
+              styles &&
+              styles(
+                { ...DNDefaultTheme, ...context.theme },
+                context.appearance,
+              )
+            }
+          />
+        )};
+      </DopebaseContext.Consumer>
+    );
+  };
+};
+
+export function extendTheme(theme) {
+  return { ...DNDefaultTheme, ...theme };
+};
+
+export function useTheme() {
+  return React.useContext(DopebaseContext);
+};
